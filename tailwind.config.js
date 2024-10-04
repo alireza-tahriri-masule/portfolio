c77c7755d8fcc39ecdee7d3f219bb0d7a53dff44
Alireza Tahriri Masule <alirezatahriri4@gmail.com>
2024-10-04 17:25:05 +0330

    feat(app): Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      english: ["Kode Mono", "system-ui"]
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+  },
+  screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
   },
   plugins: [],
 }
